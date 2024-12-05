f0eabc8571a7f6543b42eb2a1799965ff41582f0
Piengineering159 <82780222+Piengineering159@users.noreply.github.com>
2024-12-05 13:38:51 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
+  
+const { signOut } = useAuthenticator();
 const client = generateClient<Schema>();
 
 export default function App() {
@@ -52,6 +55,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
